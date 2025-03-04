6fbf5c08fa4e9238154922ff5f444ad0fc42c944
Suraj Aryal <aryalsuraj723@gmail.com>
2025-03-04 18:25:46 +0545

    Add backend feature to send messages with room

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -3,16 +3,25 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const ws_1 = require("ws");
 const wss = new ws_1.WebSocketServer({ port: 8080 });
 let sockets = [];
-wss.on('connection', (socket) => {
-    sockets.push(socket);
-    console.log(sockets.length);
-    socket.on('message', (message) => {
-        sockets.forEach(socket => {
-            socket.send(message.toString());
-        });
+wss.on("connection", (socket) => {
+    console.log(sockets);
+    socket.on("message", (message) => {
+        let parsedBody = JSON.parse(message);
+        if (parsedBody.type === "join") {
+            sockets.push({
+                socket,
+                roomId: parsedBody.payload.roomId,
+            });
+        }
+        else if (parsedBody.type === 'chat') {
+            let messageReceivers = sockets.filter(x => x.roomId === parsedBody.payload.roomId);
+            messageReceivers.forEach(receiver => {
+                receiver.socket.send(parsedBody.payload.message.toString());
+            });
+        }
     });
-    socket.on('close', (socket) => {
+    socket.on("close", (socket) => {
         // @ts-ignore
-        sockets = sockets.filter(s => s !== socket);
+        sockets = sockets.filter((s) => s !== socket);
     });
 });
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,22 +1,34 @@
-import { WebSocketServer,WebSocket } from "ws";
+import { WebSocketServer, WebSocket } from "ws";
 
-const wss = new WebSocketServer({port:8080})
+const wss = new WebSocketServer({ port: 8080 });
 
+interface User {
+  socket: WebSocket;
+  roomId: string;
+}
 
-let sockets:WebSocket[]=[]
+let sockets: User[] = [];
 
-wss.on('connection',(socket)=>{
-    sockets.push(socket)
-    console.log(sockets.length);
-    
-  socket.on('message',(message)=>{
-    sockets.forEach(socket=>{
-        socket.send(message.toString())
-    })
-  })
-  socket.on('close',(socket)=>{
+wss.on("connection", (socket) => {
+  console.log(sockets);
+
+  socket.on("message", (message) => {
+    let parsedBody = JSON.parse(message as unknown as string);
+    if (parsedBody.type === "join") {
+      sockets.push({
+        socket,
+        roomId: parsedBody.payload.roomId,
+      });
+    }
+  else if(parsedBody.type==='chat'){
+    let messageReceivers=sockets.filter(x=>x.roomId===parsedBody.payload.roomId)
+   messageReceivers.forEach(receiver=>{
+    receiver.socket.send(parsedBody.payload.message.toString())
+   })
+  }
+  });
+  socket.on("close", (socket) => {
     // @ts-ignore
-    sockets = sockets.filter(s => s !== socket)
-  })
-    
-})
+    sockets = sockets.filter((s) => s !== socket);
+  });
+});
