c3ff368b277e8eafcdb55f744a2616462015e249
Suraj Aryal <aryalsuraj723@gmail.com>
2025-03-11 16:37:37 +0545

    Add room logic for backend

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -4,24 +4,31 @@ const ws_1 = require("ws");
 const wss = new ws_1.WebSocketServer({ port: 8080 });
 let sockets = [];
 wss.on("connection", (socket) => {
-    console.log(sockets);
     socket.on("message", (message) => {
-        let parsedBody = JSON.parse(message);
-        if (parsedBody.type === "join") {
-            sockets.push({
-                socket,
-                roomId: parsedBody.payload.roomId,
-            });
+        let parsedMessage = JSON.parse(message);
+        if (parsedMessage.type === "join") {
+            if (parsedMessage.payload.roomId) {
+                sockets.push({
+                    socket,
+                    room: parsedMessage.payload.roomId,
+                });
+            }
+            else {
+                socket.send('Missing room Id');
+            }
         }
-        else if (parsedBody.type === 'chat') {
-            let messageReceivers = sockets.filter(x => x.roomId === parsedBody.payload.roomId);
-            messageReceivers.forEach(receiver => {
-                receiver.socket.send(parsedBody.payload.message.toString());
-            });
+        else if (parsedMessage.type === "chat") {
+            // @ts-ignore
+            let room = sockets.find((s) => s.socket === socket);
+            if (room) {
+                let socketsInRoom = sockets.filter((s) => (s.room === room.room));
+                socketsInRoom.forEach((s) => {
+                    s.socket.send(parsedMessage.payload.message);
+                });
+            }
+            else {
+                socket.send("You have not joined any room yet");
+            }
         }
-    });
-    socket.on("close", (socket) => {
-        // @ts-ignore
-        sockets = sockets.filter((s) => s !== socket);
     });
 });
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,34 +1,44 @@
-import { WebSocketServer, WebSocket } from "ws";
+import { WebSocket, WebSocketServer } from "ws";
 
 const wss = new WebSocketServer({ port: 8080 });
 
 interface User {
   socket: WebSocket;
-  roomId: string;
+  room: string;
 }
 
 let sockets: User[] = [];
 
 wss.on("connection", (socket) => {
-  console.log(sockets);
-
   socket.on("message", (message) => {
-    let parsedBody = JSON.parse(message as unknown as string);
-    if (parsedBody.type === "join") {
-      sockets.push({
-        socket,
-        roomId: parsedBody.payload.roomId,
-      });
+    let parsedMessage = JSON.parse(message as unknown as string);
+
+    if (parsedMessage.type === "join") {
+      if(parsedMessage.payload.roomId){
+
+        sockets.push({
+          socket,
+          room: parsedMessage.payload.roomId,
+        });
+      }else{
+        socket.send('Missing room Id')
+      }
+    } else if (parsedMessage.type === "chat") {
+      // @ts-ignore
+      let room = sockets.find((s) => s.socket === socket);
+
+      if (room) {
+        let socketsInRoom = sockets.filter((s) => (s.room === room.room));
+        socketsInRoom.forEach((s) => {
+          s.socket.send(parsedMessage.payload.message);
+        });
+      } else {
+        socket.send("You have not joined any room yet");
+      }
     }
-  else if(parsedBody.type==='chat'){
-    let messageReceivers=sockets.filter(x=>x.roomId===parsedBody.payload.roomId)
-   messageReceivers.forEach(receiver=>{
-    receiver.socket.send(parsedBody.payload.message.toString())
-   })
-  }
   });
-  socket.on("close", (socket) => {
+  socket.on('close',(socket)=>{
     // @ts-ignore
-    sockets = sockets.filter((s) => s !== socket);
-  });
+    sockets=sockets.filter(s=>s.socket!==socket)
+  })
 });
